4ecbd3facaf84371e0c2b5a9e0038197eca6a8a8
khoatran94 <39628780+khoatran94@users.noreply.github.com>
2021-07-28 19:35:52 +0200

    Update mouse_click.py

--- mouse_click.py
+++ mouse_click.py
@@ -1,37 +1,51 @@
+from absl import app, flags, logging
+from absl.flags import FLAGS
 import numpy as np
 import cv2
 
 
-mouse_coor = []
-def draw_circle(event,x,y,flags,param):
-    global mouse_coor
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img,(x,y),10,(255,0,0),5)
-        mouse_coor.append((x,y)) 
+flags.DEFINE_string('video', './data/video/cars.mp4', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('lane_config', './data/video/test.csv', 'path to the lanes coordinate points file')
+
+
 
 
 #img = cv2.imread(r'C:\Users\khoa\Desktop\github files\yolov4-deepsort-master\data\video\test.jpg')
-vid=cv2.VideoCapture(r'https://verkehrsservice.hessen.de/syncdata/video/k5370.mp4')
-_,img=vid.read()
-cv2.namedWindow("img")
-cv2.setMouseCallback("img", draw_circle)
 
-while True:
+def main(_argv):
+    try:
+        vid = cv2.VideoCapture(int(FLAGS.video))
+    except:
+        vid = cv2.VideoCapture(FLAGS.video)
+    _,img=vid.read()
+    mouse_coor = []
+    cv2.namedWindow("img")
+    def draw_circle(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            cv2.circle(img,(x,y),10,(255,0,0),5)
+            mouse_coor.append((x,y)) 
+
+    cv2.setMouseCallback("img", draw_circle)
+
+
+    while True:
     # both windows are displaying the same img
-    cv2.imshow("img", img)
-    k = cv2.waitKey(20) & 0xFF
+        cv2.imshow("img", img)
+        k = cv2.waitKey(20) & 0xFF
     
-    if k==27:
-        print(mouse_coor)
-        break
-    if k== ord('a'):
-        f=open('./data/video/coordinates.csv','w')     
-        array=np.asarray(mouse_coor)
-        for a in array:
-            f.write("%i,%i\n" %(a[0],a[1]))
-        f.close()
-        
-        # startramya change the 
-         #end of change   
-
-cv2.destroyAllWindows()
+        if k==27:
+            print(mouse_coor)
+            break
+        if k== ord('a'):
+            array=np.asarray(mouse_coor)
+            with open(FLAGS.lane_config,'w') as f:     
+                for a in array:
+                    print(f.closed)
+                    f.write("%i,%i\n" %(a[0],a[1]))
+            print(mouse_coor)
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
